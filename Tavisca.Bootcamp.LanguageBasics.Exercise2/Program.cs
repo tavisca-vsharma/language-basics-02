042d8f23ce2301a1f6231a956131e78567bd181b
Nikhil Prasad <nikhil.prasad@gmail.com>
2019-06-19 20:28:31 +0530

    Added boilerplate code for problem.

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
+            Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
+            Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
+            Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
+            Console.ReadKey(true);
+        }
+
+        private static void Test(string[] postTimes, string[] showTimes, string expected)
+        {
+            var result = GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
+            var postTimesCsv = string.Join(", ", postTimes);
+            var showTimesCsv = string.Join(", ", showTimes);
+            Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
+        }
+
+        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+        {
+            // Add your code here.
+            throw new NotImplementedException();
+        }
+    }
+}
